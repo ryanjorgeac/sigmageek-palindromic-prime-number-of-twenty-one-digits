b9f11f99a12d4e56ccdc3dc3943cc6e367304bc3
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-09-14 20:11:10 -0300

    Main changed for testing. Not to worry about.

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from isPrime import isPrime
 from twentyOneDigitsGenerator import twentyOneDigitsGenerator
 
 if __name__ == "__main__":
-    generator = piGenerator()
+    generator = piGenerator.piGenerator(5000)
     x = twentyOneDigitsGenerator(generator)
     i = 0
+    number1 = 0
     for number in x:
-        if i > 1000000:
+        if i == 0:
             print("I have excedeed my limit. Please, use a better generator.")
+            print(number)
             break
         if isPalindromic(number) and isPrime(number):
             print(i)
             print(number)
             break
-        i+=1
+        i += 1
