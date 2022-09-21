428c61aaba4224a72636a4356638cf10ecb7b67e
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-09-21 20:20:36 -0300

    New tests created.

--- test_piGenerator.py
+++ test_piGenerator.py
@@ -1,18 +1,21 @@
 import piGenerator
 
 
-def test_piGenerator1():
-    x = piGenerator.piGenerator(0)
-    y = x.__next__()
-    assert y == 3
+def test_piGenerator_10_digits():
+    x = piGenerator.piGenerator(0, 10)
+    y = list(x)
+    assert len(y) == 10
 
-def test_piGenerator2():
-    x = piGenerator.piGenerator(0)
-    y = 0
-    for i in range(4):
-        y += x.__next__()
-    assert y == 9
+def test_piGenerator_more_than_1000_digits():
+    x = piGenerator.piGenerator(0, 1050)
+    y = list(x)
+    assert len(y) == 1050
+
+def test_piGenerator_more_than_5000_digits():
+    x = piGenerator.piGenerator(0, 5000)
+    y = list(x)
+    assert len(y) == 5000
 
 
 if __name__ == "__main__":
-    test_piGenerator2()
+    test_piGenerator_more_than_5000_digits()
