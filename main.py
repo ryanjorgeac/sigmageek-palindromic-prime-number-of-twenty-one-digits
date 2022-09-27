0165cb9c0ab7176035534ea3899850e5aef15ba1
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-09-27 15:31:05 -0300

    Main and multiprocessingConfig used for testing.

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import piGenerator
 from isPalindromic import isPalindromic
 from isPrime import isPrime
 from anyNumberOfDigitsGenerator import anyNumberOfDigitsGenerator
+import requests
 
 if __name__ == "__main__":
-    generator = piGenerator.piGenerator(0, 1000000)
-    x = anyNumberOfDigitsGenerator(generator, 21)
+    generator = piGenerator.piGenerator(0, 150000)
+    x = anyNumberOfDigitsGenerator(generator, 9)
     i = 0
     for number in x:
-        if number % 2 != 0:
-            if number % 5 != 0:
-                if isPalindromic(number) and isPrime(number):
-                    print(i)
-                    print("Palindromic and prime Founder: ", number)
-                    break
-        elif i == 1000000:
-            print(number, "posição: ", i)
-            break
-        i += 1
+        # if number % 2 != 0:
+        #     if number % 5 != 0:
+        #         if isPalindromic(number) and isPrime(number):
+        #             print(i)
+        #             print("Palindromic and prime Founder: ", number)
+        #             break
+        # elif i == 150000:
+        #     print(number, "posição: ", i)
+        #     break
+        # i += 1
+        print(number)
+
--- multiprocessingConfig.py
+++ multiprocessingConfig.py
@@ -6,6 +6,7 @@ import isPrime
 import piGenerator
 import anyNumberOfDigitsGenerator
 
+
 def moreMillion():
     million = 1000000
     yield million
@@ -13,13 +14,19 @@ def moreMillion():
         million += 1000000
         yield million
 
+
 def findPalindromicInPi(start, end, numberOfDigits):
     generator = piGenerator.piGenerator(start, end)
     x = anyNumberOfDigitsGenerator.anyNumberOfDigitsGenerator(generator, numberOfDigits)
-    i = 0
+    i = start
     for number in x:
-        print(number)
+        if isPrime.isPrime(number):
+            if isPalindromic.isPalindromic(number):
+                print(i)
+                print("Palindromic and prime found: ", number)
+                break
         i += 1
+
 
 if __name__ == "__main__":
     # with Pool(3) as p:
@@ -43,18 +50,18 @@ if __name__ == "__main__":
     #         i += 1]
     start = time.perf_counter()
 
-    p1 = multiprocessing.Process(target=findPalindromicInPi(0, 100, 4))
-    p2 = multiprocessing.Process(target=findPalindromicInPi(50000, 100000, 4))
-    p3 = multiprocessing.Process(target=findPalindromicInPi(100000, 150000, 4))
+    # p1 = multiprocessing.Process(target=findPalindromicInPi(0, 100, 4))
+    p2 = multiprocessing.Process(target=findPalindromicInPi(0, 150000, 9))
+    # p3 = multiprocessing.Process(target=findPalindromicInPi(100000, 150000, 4))
 
-    p1.start()
+    # p1.start()
     p2.start()
-    p3.start()
+    # p3.start()
 
-    p1.join()
+    # p1.join()
     p2.join()
-    p3.join()
+    # p3.join()
 
     finish = time.perf_counter()
 
-    print(f'Finished in {round(finish-start, 2)} second(s)')
+    print(f'Finished in {round(finish - start, 2)} second(s)')
