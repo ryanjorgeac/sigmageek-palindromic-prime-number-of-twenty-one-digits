6bc8f607fe44517740988eb9ec2d46dc428f7d9f
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-09-26 21:25:45 -0300

    Zero problems tested and one solved. The last test still fail, so the problem's still in the code.

--- test_anyNumberOfDigitsGenerator.py
+++ test_anyNumberOfDigitsGenerator.py
@@ -20,3 +20,24 @@ def test_50_digits_generator():
     generator = fakeGenerator([1,4,7,0,2])
     x = anyNumberOfDigitsGenerator(generator, 50).__next__()
     assert x == 14702147021470214702147021470214702147021470214702
+
+def test_number_with_zero_on_the_road():
+    generator = fakeGenerator([1, 0, 2, 3, 4, 5])
+    listNumbers = []
+    x = anyNumberOfDigitsGenerator(generator, 2)
+    for i in range(5):
+        z = x.__next__()
+        listNumbers.append(z)
+    assert listNumbers[1] == 23
+
+def test_number_starting_with_zero():
+    generator = fakeGenerator([0, 1, 2, 3, 4, 5])
+    listNumbers = []
+    x = anyNumberOfDigitsGenerator(generator, 2)
+    for i in range(5):
+        z = x.__next__()
+        listNumbers.append(z)
+    assert listNumbers[0] == 12
+
+if __name__ == "__main__":
+    test_50_digits_generator()
