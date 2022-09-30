c23f9bc7184b0da7dec209807bc616c29419138e
Ryan Carvalho <ryan.abreu@dcx.ufpb.br>
2022-09-29 22:02:32 -0300

    MultiprocessingConfig changed for better. Now it's going to work with Either objects inside the Pool. It's not completed yet.

--- multiprocessingConfig.py
+++ multiprocessingConfig.py
@@ -1,43 +1,16 @@
-import multiprocessing
 from multiprocessing import Pool
 import time
-import isPalindromic
-import isPrime
-from hasNDigits import hasNDigits
-import piGenerator
-import anyNumberOfDigitsGenerator
-
-
-def findPalindromicInPi(start, end, numberOfDigits):
-    generator = piGenerator.piGenerator(start, end)
-    x = anyNumberOfDigitsGenerator.anyNumberOfDigitsGenerator(generator, numberOfDigits)
-    i = start
-    for number in x:
-        assert hasNDigits(number, numberOfDigits)
-        if isPrime.isPrime(number) and isPalindromic.isPalindromic(number):
-            print("Palindromic and prime found: ", number, " and your position::: ", i)
-            break
-        i += 1
-
+from aggregateNumbers import aggregateNumbers
+from poolArgsGenerator import poolArgsGenerator
+from findPalindromicInPi import findPalindromicInPi
 
 if __name__ == "__main__":
     start = time.perf_counter()
+    argsGenerator = poolArgsGenerator(21)
+    interval = next(argsGenerator)
     with Pool() as p:
         # use the apply function
-        pass
+        while flag: # flag not set yet
+            p.apply_async(findPalindromicInPi, args=interval, callback=aggregateNumbers)
+            interval = next(argsGenerator)
 
-    p1 = multiprocessing.Process(target=findPalindromicInPi, args=(0, 100000, 5))
-    p2 = multiprocessing.Process(target=findPalindromicInPi, args=(100000, 200000, 5))
-    p3 = multiprocessing.Process(target=findPalindromicInPi, args=(100000, 300000, 5))
-
-    p1.start()
-    p2.start()
-    p3.start()
-
-    p1.join()
-    p2.join()
-    p3.join()
-
-    finish = time.perf_counter()
-
-    print(f'Finished in {round(finish - start, 2)} second(s)')
